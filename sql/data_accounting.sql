71594f5cd3525dad0744359157f0a41f7f31d58b
rht <rhtbot@protonmail.com>
2021-05-16 03:38:58 -0400

    sql: Update data type of columns

--- sql/data_accounting.sql
+++ sql/data_accounting.sql
@@ -7,10 +7,10 @@ CREATE TABLE IF NOT EXISTS `page_verification` (
 	`page_verification_id` INT(32) NOT NULL AUTO_INCREMENT, 
 	`page_id` INT COMMENT 'from page table',
 	`rev_id` INT COMMENT 'from revision table',
-	`hash_content` INT(128) COMMENT 'Hashing the page content of the current version',
-	`hash_metadata` INT(128) COMMENT 'Hashing all values of related revision_id tuble entry in 
+	`hash_content` VARCHAR(128) COMMENT 'Hashing the page content of the current version',
+	`hash_metadata` VARCHAR(128) COMMENT 'Hashing all values of related revision_id tuble entry in 
 revision table',
-	`hash_verification` INT(128) COMMENT 'Combined metadata and content hash',
+	`hash_verification` VARCHAR(128) COMMENT 'Combined metadata and content hash',
 	`signature` VARCHAR(128),
 	`public_key` VARCHAR(128),
 	PRIMARY KEY (`page_verification_id`)
@@ -23,8 +23,8 @@ CREATE TABLE IF NOT EXISTS `page_witness` (
 	`rev_id` INT,
 	`page_verification_id` INT COMMENT 'from page_verification table',
 	`witness_network` VARCHAR(18) DEFAULT 'Ethereum',
-	`network_transaction_id` INT(128),
-	`merkle_root` INT COMMENT 'Merkle Root Hash',
+	`network_transaction_id` VARCHAR(128),
+	`merkle_root` VARCHAR(128) COMMENT 'Merkle Root Hash',
 	PRIMARY KEY (`page_witness_id`)
 );
 
