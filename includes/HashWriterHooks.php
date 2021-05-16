8d19d94fe881595523e21148cdc0c4e1d8e52513
rht <rhtbot@protonmail.com>
2021-05-16 04:32:13 -0400

    Update HashWriterHooks

--- includes/HashWriterHooks.php
+++ includes/HashWriterHooks.php
@@ -10,6 +10,18 @@ namespace MediaWiki\Extension\Example;
 use MediaWiki\Revision\SlotRecord;
 use DatabaseUpdater;
 
+function getHashSum($inputStr) {
+	return hash("sha3-512", $inputStr);
+}
+
+function calculateMetadataHash($timestamp, $verificationHash = "", $signature = "", $publicKey = "") {
+	return getHashSum($timestamp);
+}
+
+function calculateVerificationHash($contentHash, $metadataHash) {
+	return getHashSum($contentHash + $metadataHash);
+}
+
 class HashWriterHooks implements
 	\MediaWiki\Page\Hook\RevisionFromEditCompleteHook,
 	\MediaWiki\Installer\Hook\LoadExtensionSchemaUpdatesHook
@@ -17,8 +29,18 @@ class HashWriterHooks implements
 
 	public function onRevisionFromEditComplete( $wikiPage, $rev, $originalRevId, $user, &$tags ) {
 		$dbw = wfGetDB( DB_MASTER );
+		$pageContent = $rev->getContent(SlotRecord::MAIN)->serialize();
+		$contentHash = getHashSum($pageContent);
+		$metadataHash = calculateMetadataHash($rev->getTimeStamp());
+		$parentId = $rev->getParentId()
 		$data = [
-			'rev_id' => 2,
+			'page_id' => 2,
+			'rev_id' => $rev->getID(),
+			'hash_content' => $contentHash,
+			'hash_metadata' => $metadataHash,
+			'hash_verification' => calculateVerificationHash($contentHash, $metadataHash),
+			'signature' => substr($pageContent, 0, 10),
+			'public_key' => $parentId,
 		];
 		$dbw->insert('page_verification', $data, __METHOD__);
 	}
