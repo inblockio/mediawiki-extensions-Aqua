9cf36bf534e98efe97d4da7374d8c52a26c0b2c2
Timotheus <tim.bansemer@posteo.net>
2021-05-18 14:33:37 +0800

    Updated RESTAPI codebase wfGetDB with new function https://www.mediawiki.org/wiki/Manual:Database_access REMARK: Add use MediaWiki\MediaWikiServices; AND use DB_MASTER, not DB_PRIMARY.

--- includes/RestApiDataAccounting.php
+++ includes/RestApiDataAccounting.php
@@ -4,6 +4,7 @@ namespace MediaWiki\Extension\Example;
 
 use MediaWiki\Rest\SimpleHandler;
 use Wikimedia\ParamValidator\ParamValidator;
+use MediaWiki\MediaWikiServices;
 
 /**
  * Example class to echo a path parameter
@@ -18,9 +19,15 @@ class RestApiDataAccounting extends SimpleHandler {
 			case 'get_hash':
 				return base64_decode( $valueToEcho );
 
+			/**
+			 *the 'data_input' method will take the base64 string and decode it. After decoding it there should be a string which contains the input data from the Metamask singing process [$Singature,$PublicKey] whic are then writtein into the database page_verification under the respective datafields.
+			 */
 			case 'data_input':
-				$dbw = wfGetDB( DB_MASTER );
-				$data = ['hash_content' => "it works!"];
+				#$dbw = wfGetDB( DB_MASTER );
+				$lb = MediaWikiServices::getInstance()->getDBLoadBalancer();
+				$dbw = $lb->getConnectionRef( DB_MASTER );
+				$data = ['hash_content' => "New Entry!"];
+				
 				$dbw->insert('page_verification', $data, __METHOD__);
 				return base64_decode( $valueToEcho );
 
