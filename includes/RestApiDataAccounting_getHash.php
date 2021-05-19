c479bc83b1844a573c01779e9838d7df205e8a22
Timotheus <tim.bansemer@posteo.net>
2021-05-19 08:09:44 +0800

    Change two use two API_Methods and Paths: data_input and get_hash to ensure to validate input correctly. API: SQL QUERY for READING DB still Fails. Code is clean with example and documentation. Therefore Code is deactivated. Remove whitespace characters.

--- includes/RestApiDataAccounting_getHash.php
+++ includes/RestApiDataAccounting_getHash.php
@@ -6,26 +6,23 @@ use MediaWiki\Rest\SimpleHandler;
 use Wikimedia\ParamValidator\ParamValidator;
 use MediaWiki\MediaWikiServices;
 
-/**
- * Example class to echo a path parameter
- */
 class RestApiDataAccounting_getHash extends SimpleHandler {
 
 	/** @inheritDoc */
 	public function run( $rev_id ) {
 
-		 /* *
-                 * $dbr = $lb->getConnectionRef( DB_REPLICA );
-                 * $res = $dbr->select(
-                 * 'category',                              // $table The table to query FROM (or array of tables)
-                 * [ 'cat_title', 'cat_pages' ],            // $vars (columns of the table to SELECT)
-                 * 'cat_pages > 0',                         // $conds (The WHERE conditions)
-                 * __METHOD__,                              // $fname The current __METHOD__ (for performance tracking)
-                 * [ 'ORDER BY' => 'cat_title ASC' ]        // $options = []
-                 * );
-                 * This example corresponds to the query
-                 * SELECT cat_title, cat_pages FROM category WHERE cat_pages > 0 ORDER BY cat_title ASC
-                **/
+		/**
+		 * $dbr = $lb->getConnectionRef( DB_REPLICA );
+		 * $res = $dbr->select(
+		 * 'category',                              // $table The table to query FROM (or array of tables
+		 * [ 'cat_title', 'cat_pages' ],            // $vars (columns of the table to SELECTED
+		 * 'cat_pages > 0',                         // $conds (The WHERE conditions)
+		 * __METHOD__,                              // $fname The current __METHOD__ (for performance tracking)
+		 * [ 'ORDER BY' => 'cat_title ASC' ]        // $options = []
+		 * );
+		 * This example corresponds to the query
+		 * SELECT cat_title, cat_pages FROM category WHERE cat_pages > 0 ORDER BY cat_title ASC
+		 * **/
 
 		$lb = MediaWikiServices::getInstance()->getDBLoadBalancer();
 		$dbr = $lb->getConnectionRef( DB_REPLICA );
