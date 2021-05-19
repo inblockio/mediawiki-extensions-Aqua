d08ca45044a2ff17960893a3f900708b38d3af34
Timotheus <tim.bansemer@posteo.net>
2021-05-19 08:09:44 +0800

    Change two use two API_Methods and Paths: data_input and get_hash to ensure to validate input correctly. API: SQL QUERY for READING DB still Fails. Code is clean with example and documentation. Therefore Code is deactivated. Remove whitespace characters.

--- includes/RestApiDataAccounting.php
+++ includes/RestApiDataAccounting.php
@@ -10,15 +10,20 @@ class RestApiDataAccounting extends SimpleHandler {
 
 	/** @inheritDoc */
 	public function run( $signature, $public_key ) {
+			/** include functionality to write to database. 
+			* See https://www.mediawiki.org/wiki/Manual:Database_access */
 			$lb = MediaWikiServices::getInstance()->getDBLoadBalancer();
 			$dbw = $lb->getConnectionRef( DB_MASTER );
 			
 			$table = 'page_verification';
+				
+			/** prepare data input */
 			$field = 'signature';
 			$data = base64_decode( $signature );
 			$field_two = 'public_key';
 			$data_two = base64_decode( $public_key );
 
+			/** write data to database */
 			$dbw->insert($table ,[$field => $data,$field_two => $data_two], __METHOD__);
 					
 		return ( "Store of Signature {$signature} and Public Key {$public_key} Successful!"  );
