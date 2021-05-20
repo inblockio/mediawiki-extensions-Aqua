d5472496f59663cfde67c45d62eaad0f94edfc3f
Timotheus <tim.bansemer@posteo.net>
2021-05-20 12:45:58 +0800

    Initilize all veriables to zero after DB write. Mauelly confirmed that all specified tests pass. See verified page history in the developer wiki, section tests 1-5.

--- includes/HashWriterHooks.php
+++ includes/HashWriterHooks.php
@@ -66,6 +66,14 @@ class HashWriterHooks implements
 			'debug' => $rev->getTimeStamp() . ' previous hash verification ' . $metadata[0] . ' signature ' . $metadata[1] . ' public key ' . $metadata[2] . ' content ' . substr($pageContent, 0, 10),
 		];
 		$dbw->insert('page_verification', $data, __METHOD__);
+		/** re-initilizing variables to ensure they do not hold values for the next revision. */
+		$rev_id =[];
+		$pageContent =[];
+		$contentHash =[];
+		$parentId =[];
+		$metadata =[];
+		$metadataHash =[];
+		$data =[];	
 	}
 
 	/**
