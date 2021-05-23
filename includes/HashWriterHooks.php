0b4bb49fb08a8c0b9ede7aec7cbf42570d42e316
rht <rhtbot@protonmail.com>
2021-05-23 01:18:35 -0400

    hook: Enable PHP debugging

--- includes/HashWriterHooks.php
+++ includes/HashWriterHooks.php
@@ -7,6 +7,10 @@
 
 namespace MediaWiki\Extension\Example;
 
+# include / exclude for debugging
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
 use MediaWiki\Revision\SlotRecord;
 use DatabaseUpdater;
 
