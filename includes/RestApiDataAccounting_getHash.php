558ded47bbb0922c15455ece05e2a27aa6f270fd
Timotheus <tim.bansemer@posteo.net>
2021-05-23 11:28:47 +0800

    Test and Change

--- includes/RestApiDataAccounting_getHash.php
+++ includes/RestApiDataAccounting_getHash.php
@@ -1,10 +1,6 @@
 <?php
 namespace MediaWiki\Extension\Example;
 
-# include / exclude for debugging
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
-
 use MediaWiki\Rest\SimpleHandler;
 use Wikimedia\ParamValidator\ParamValidator;
 use MediaWiki\MediaWikiServices;
@@ -13,19 +9,6 @@ class RestApiDataAccounting_getHash extends SimpleHandler {
 
 	/** @inheritDoc */
 	public function run( $rev_id ) {
-
-		/**
-		 * $dbr = $lb->getConnectionRef( DB_REPLICA );
-		 * $res = $dbr->select(
-		 * 'category',                              // $table The table to query FROM (or array of tables
-		 * [ 'cat_title', 'cat_pages' ],            // $vars (columns of the table to SELECTED
-		 * 'cat_pages > 0',                         // $conds (The WHERE conditions)
-		 * __METHOD__,                              // $fname The current __METHOD__ (for performance tracking)
-		 * [ 'ORDER BY' => 'cat_title ASC' ]        // $options = []
-		 * );
-		 * This example corresponds to the query
-		 * SELECT cat_title, cat_pages FROM category WHERE cat_pages > 0 ORDER BY cat_title ASC
-		 * **/
 
 		$lb = MediaWikiServices::getInstance()->getDBLoadBalancer();
 		$dbr = $lb->getConnectionRef( DB_REPLICA );
@@ -39,8 +22,11 @@ class RestApiDataAccounting_getHash extends SimpleHandler {
 
 		$output = '';
 		foreach( $res as $row ) {
-        	$output .= 'Revision[' . $row->rev_id . '] PageVerificationHash[' . $row->hash_verification .']';
+        	#$output .= 'Revision[' . $row->rev_id . '] PageVerificationHash[' . $row->hash_verification .']';
+		$output = 'Test'; #'0x'. $row->hash_verification .'';
+		#$output = substr($output, 1, -1);
 		}
+
 
 
 		#This code corresponds to the query
