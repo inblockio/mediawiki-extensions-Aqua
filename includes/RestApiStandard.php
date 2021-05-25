619c90e2dc2c9d8b28ff51573b01348282ff2610
Timotheus <tim.bansemer@posteo.net>
2021-05-25 11:23:41 +0800

    Add and test all actions required to the Rest API

--- includes/RestApiStandard.php
+++ includes/RestApiStandard.php
@@ -10,19 +10,42 @@ use Wikimedia\ParamValidator\ParamValidator;
  */
 class RestApiStandard extends SimpleHandler {
 
-	private const VALID_ACTIONS = [ 'reverse', 'shuffle', 'md5' ];
+	private const VALID_ACTIONS = [ 'rev_id', 'give_page', 'page_all_rev', 'page_last_rev', 'page_last_rev_sig', 'page_all_rev_sig', 'page_all_rev_wittness', 'page_all_rev_sig_witness' ];
 
 	/** @inheritDoc */
 	public function run( $action, $var1, $var2 ) {
 		switch ( $action ) {
-			case 'reverse':
-				return [ 'echo' => strrev( $var1 ) . $var2 ];
+                        #Expects rev_id: $var1 as input and returns verification_hash(required), signature(optional), public_key(optional), wallet_address(optiona    l), witness_id(optional)
+                        case 'rev_id':
+				return [ "Expects rev_id: $var1 as input and returns verification_hash(required), signature(optional), public_key(optional), wallet_address(optional), witness_id(optional) " ];
 
-			case 'shuffle':
-				return [ 'echo' => str_shuffle( $var1 ) ];
+                        #Expects rev_id: $var1 as input and returns page_title and page_id
+                        case 'give_page':
+				return [ "Expects rev_id: $var1 as input and returns page_title and page_id" ];
 
-			case 'md5':
-				return [ 'echo' => md5( $var1 ) ];
+                        #Expects page title: $var1 and returns LAST verified revision
+                        case 'page_last_rev':
+				return [ "Expects page title: $var1 and returns LAST verified revision"];
+
+                        #Expects page title: $var1 and returns LAST verified and signed revision
+                        case 'page_last_rev_sig':
+				return [ "Expects page title: $var1 and returns LAST verified and signed revision"];
+
+                        i#Expects page title: $var1 and returns ALL verified revisions
+                        case 'page_all_rev':
+				return [ "Expects page title: $var1 and returns ALL verified revisions"];
+
+                        #Expects page title: $var1 and returns ALL verified revisions which have been signed
+			case 'page_all_rev_sig':
+				return [ "Expects page title: $var1 and returns ALL verified revisions which have been signed"];
+
+                        #Expects page title: $var1 and returns ALL verified revisions which have been witnessed
+			case 'page_all_rev_wittness':
+				return [ "Expects page title: $var1 and returns ALL verified revisions which have been witnessed"];
+
+                        #Expects page title: '$var1' and returns ALL verified revisions which have been signed and wittnessed
+			case 'page_all_rev_sig_witness':
+				return [ "Expects page title: $var1 and returns ALL verified revisions which have been signed and wittnessed"];
 
 			default:
 				return [ ' echo' => $valueToEcho ];
