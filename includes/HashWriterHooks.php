f7c0b74880b3393786a9ab5f1cd07ce997f3dc6e
Timotheus <tim.bansemer@posteo.net>
2021-05-30 11:09:48 +0800

    Autoformat File

--- includes/HashWriterHooks.php
+++ includes/HashWriterHooks.php
@@ -16,85 +16,85 @@ use DatabaseUpdater;
 use MediaWiki\MediaWikiServices;
 
 function getHashSum($inputStr) {
-	return hash("sha3-512", $inputStr);
+    return hash("sha3-512", $inputStr);
 }
 
 function calculateMetadataHash($timestamp, $previousVerificationHash = "", $signature = "", $publicKey = "") {
-	return getHashSum($timestamp . $previousVerificationHash . $signature . $publicKey);
+    return getHashSum($timestamp . $previousVerificationHash . $signature . $publicKey);
 }
 
 function calculateVerificationHash($contentHash, $metadataHash) {
-	return getHashSum($contentHash . $metadataHash);
+    return getHashSum($contentHash . $metadataHash);
 }
 
 function makeEmptyIfNonce($x) {
-	return ($x == "nonce") ? "": $x;
+    return ($x == "nonce") ? "": $x;
 }
 
 function getPageVerificationData($dbr, $previous_rev_id) {
-	$res = $dbr->select(
-		'page_verification',
-		[ 'rev_id', 'hash_verification', 'signature', 'public_key' ],
-		"rev_id = $previous_rev_id",
-		__METHOD__
-	);
-	$output = array();
-	foreach( $res as $row) {
-		array_push($output, makeEmptyIfNonce($row->hash_verification));
-		array_push($output, makeEmptyIfNonce($row->signature));
-		array_push($output, makeEmptyIfNonce($row->public_key));
-		array_push($output, makeEmptyIfNonce($row->wallet_address));
-		break;
-	}
-	return $output;
+    $res = $dbr->select(
+        'page_verification',
+        [ 'rev_id', 'hash_verification', 'signature', 'public_key' ],
+        "rev_id = $previous_rev_id",
+        __METHOD__
+    );
+    $output = array();
+    foreach( $res as $row) {
+        array_push($output, makeEmptyIfNonce($row->hash_verification));
+        array_push($output, makeEmptyIfNonce($row->signature));
+        array_push($output, makeEmptyIfNonce($row->public_key));
+        array_push($output, makeEmptyIfNonce($row->wallet_address));
+        break;
+    }
+    return $output;
 }
 
 class HashWriterHooks implements
-	\MediaWiki\Page\Hook\RevisionFromEditCompleteHook,
-	\MediaWiki\Installer\Hook\LoadExtensionSchemaUpdatesHook
+    \MediaWiki\Page\Hook\RevisionFromEditCompleteHook,
+    \MediaWiki\Installer\Hook\LoadExtensionSchemaUpdatesHook
 {
 
-	public function onRevisionFromEditComplete( $wikiPage, $rev, $originalRevId, $user, &$tags ) {
+    public function onRevisionFromEditComplete( $wikiPage, $rev, $originalRevId, $user, &$tags ) {
         $lb = MediaWikiServices::getInstance()->getDBLoadBalancer();
         $dbw = $lb->getConnectionRef( DB_MASTER );
 
-		$pageContent = $rev->getContent(SlotRecord::MAIN)->serialize();
-		$contentHash = getHashSum($pageContent);
-		$parentId = $rev->getParentId();
-		$metadata = getPageVerificationData($dbw, $parentId);
+        $pageContent = $rev->getContent(SlotRecord::MAIN)->serialize();
+        $contentHash = getHashSum($pageContent);
+        $parentId = $rev->getParentId();
+        $metadata = getPageVerificationData($dbw, $parentId);
         $timestamp = $rev->getTimeStamp();
-		$metadataHash = calculateMetadataHash($timestamp, $metadata[0], $metadata[1], $metadata[2]);
-		$data = [
-			'page_title' => $wikiPage->getTitle(),
-			'page_id' => $wikiPage->getId(),
-			'rev_id' => $rev->getID(),
-			'hash_content' => $contentHash,
+        $metadataHash = calculateMetadataHash($timestamp, $metadata[0], $metadata[1], $metadata[2]);
+        $data = [
+            'page_title' => $wikiPage->getTitle(),
+            'page_id' => $wikiPage->getId(),
+            'rev_id' => $rev->getID(),
+            'hash_content' => $contentHash,
             'time_stamp' => $timestamp,
             'hash_metadata' => $metadataHash,
-			'hash_verification' => calculateVerificationHash($contentHash, $metadataHash),
-			'signature' => '',
-			'public_key' => '',
-                        'wallet_address' => '', 
-			'debug' => $rev->getTimeStamp().'[PV]'.$metadata[0].'[SIG]'.$metadata[1].'[PK]'.$metadata[2]
-		];
-		$dbw->insert('page_verification', $data, __METHOD__);
-                /** re-initilizing variables to ensure they do not hold values for the next revision. */
-		$rev_id =[];
-		$pageContent =[];
-		$contentHash =[];
-		$parentId =[];
-		$metadata =[];
-		$metadataHash =[];
-		$data =[];
-	}
+            'hash_verification' => calculateVerificationHash($contentHash, $metadataHash),
+            'signature' => '',
+            'public_key' => '',
+            'wallet_address' => '', 
+            'debug' => $rev->getTimeStamp().'[PV]'.$metadata[0].'[SIG]'.$metadata[1].'[PK]'.$metadata[2]
+        ];
+        $dbw->insert('page_verification', $data, __METHOD__);
+        /** re-initilizing variables to ensure they do not hold values for the next revision. */
+        $rev_id =[];
+        $pageContent =[];
+        $contentHash =[];
+        $parentId =[];
+        $metadata =[];
+        $metadataHash =[];
+        $data =[];
+    }
 
-	/**
-	 * Register our database schema.
-	 *
-	 * @see https://www.mediawiki.org/wiki/Manual:Hooks/LoadExtensionSchemaUpdates
-	 * @param DatabaseUpdater $updater
-	 */
-	public function onLoadExtensionSchemaUpdates( $updater ) {
-		$updater->addExtensionTable( 'data_accounting', dirname( __DIR__ ) . '/sql/data_accounting.sql' );
-	}
+    /**
+     * Register our database schema.
+     *
+     * @see https://www.mediawiki.org/wiki/Manual:Hooks/LoadExtensionSchemaUpdates
+     * @param DatabaseUpdater $updater
+     */
+    public function onLoadExtensionSchemaUpdates( $updater ) {
+        $updater->addExtensionTable( 'data_accounting', dirname( __DIR__ ) . '/sql/data_accounting.sql' );
+    }
 }
