ad2e801a1f292696d719a2f1b7ccf692bc22704f
Timotheus <tim.bansemer@posteo.net>
2021-07-14 01:28:38 +0200

    Add new SpecialPages to seperate functionality for increased structure of Module 4: witnessing and to easier mantain the codebase. Added pseudocode to explain future functionality within the pages.

---
+++ includes/SpecialDataAccountingConfig.php
@@ -0,0 +1,17 @@
+<php?
+
+/**
+ * Behavior description of Special Page Data Accounting Config
+ * All configuration settings for all modules are set here. Following settings can be adujusted:
+ * Module 1: Page Verification
+ *          No Settings
+ * Module 2: Verify Page History 
+ *          No Settings - Is now the external verifier. Settings for the external verifier have been moved into the chrome extension.
+ * Module 3: Export / Import of Verified Page History
+ *          No Settings
+ * Module 4: Witnessing
+ *          Set Witness Smart Contract (For SpecialPage:WitnessPublisher)
+ *          Set Witness Network (For SpecialPage:WitnessPublisher)
+ */
+
+?>
---
+++ includes/SpecialWitnessPublisher.php
@@ -0,0 +1,8 @@
+<php?
+
+/**
+ * Behaviral description of SpecialPage:WitnessPublisher
+ * The SpecialPage prints out the database witness_events in decending order (starting with the latest witness event). If a page manifest was not yet published, the page should hold a publish button instead of the empty field for 'page_witness_transaction_hash'. In this list it is easy to see how many Witness Events have taken place and which receipts in the form of the page_manifests have been generated and whats their page link (they should be clickable to be redireted to the respective page manifest).
+ */
+
+?>
