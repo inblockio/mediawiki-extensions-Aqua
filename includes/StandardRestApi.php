fc5727357b44444ef29b956002bf2682f340f43b
Timotheus <tim.bansemer@posteo.net>
2021-08-14 11:24:07 +0700

    Adding docs and basic documentation for API; Cleanup of StandardAPI

--- includes/StandardRestApi.php
+++ includes/StandardRestApi.php
@@ -31,7 +31,17 @@ function selectToArray($db, $table, $col, $conds) {
  */
 class StandardRestApi extends SimpleHandler {
 
-    private const VALID_ACTIONS = ['help', 'verify_page', 'get_page_by_rev_id', 'page_all_rev', 'get_page_last_rev', 'page_last_rev_sig', 'get_page_all_rev_sig', 'get_witness_data', 'request_merkle_proof', 'store_signed_tx', 'store_witness_tx', 'request_hash' ];
+    private const VALID_ACTIONS = [ 
+        'verify_page', 
+        'get_page_by_rev_id', 
+        'page_all_rev', 
+        'get_page_last_rev', 
+        'get_witness_data', 
+        'request_merkle_proof', 
+        'store_signed_tx', 
+        'store_witness_tx', 
+        'request_hash' 
+    ];
 
     /** @inheritDoc */
     public function run( $action ) {
@@ -42,26 +52,7 @@ class StandardRestApi extends SimpleHandler {
         $var4 = $params['var4'] ?? null;
         switch ( $action ) {
             #Expects rev_id as input and returns verification_hash(required), signature(optional), public_key(optional), wallet_address(optional), witness_id(optional)
-        case 'help':
-            $index = (int) $var1;
-            $output = array();
-            $output[0]=["help expects an number as in input to displays information for all actions this API services.To use an action, replace 'help' in your url with your desired action. ensure you provide all four '\' as they are the separators for up to four input variables. actions: help[0], verify_page[1], get_page_by_rev_id[2], get_page_last_rev[3], page_last_rev_sig[4], page_all_rev[5], get_page_all_rev_sig[6], request_merkle_proof[7], get_witness_data[8], store_signed_tx[9], store_witness_tx[10]"];
-            $output[1]=['action \'verify_page\': expects revision_id as input and returns domain_id, verification_hash(required), signature(optional), public_key(optional), wallet_address(optional), witness_id(optional)'];
-            $output[2]=['action \'get_page_by_rev_id\': expects revision_id as input and returns page_title and page_id'];
-            $output[3]=['action \'get_page_last_rev\': expects page_title and returns last verified revision.'];
-            $output[4]=['action \'page_lage_rev_sig\': expects page_title as input and returns last signed and verified revision_id.'];
-            $output[5]=['action \'page_all_rev\': expects page_title as input and returns last signed and verified revision_id.'];
-            $output[6]=['action \'get_page_all_rev_sig\':NOT IMPLEMENTED'];
-            $output[7]=['action \'request_merkle_proof\':expects witness_id and page_verification hash and returns left_leaf,righ_leaf and successor hash to verify the merkle proof node by node, data is retrieved from the witness_merkle_tree db. Note: in some cases there will be multiple replays to this query. In this case it is required to use the depth as a selector to go through the different layers. Depth can be specified via the $depth parameter'];
-            $output[8]=['action \'get_witness_data\' - expects page_witness_id - used to retrieve all required data to execute a witness event (including witness_event_verification_hash, network ID or name, witness smart contract address) for the publishing via Metamask'];
-            $output[9]=['action \'store_signed_tx\':expects revision_id=value1 [required] signature=value2[required], public_key=value3[required] and wallet_address=value4[required] as inputs; Returns a status for success or failure
-                '];
-            $output[10]=['action \'store_witness_tx\' expects witness id: $var1; account_address:$var2; transaction_id:$var3 and returns success or error code, used to receive data from metamask'];
-
-
-            return $output[$index];
-
-        case 'verify_page':
+                case 'verify_page':
 
             $rev_id = $var1;
 
@@ -144,37 +135,11 @@ class StandardRestApi extends SimpleHandler {
             }
             return $output;
 
-            #Expects Page Title Name as INPUT and RETURNS LAST signed (and verified) revision.
-            #Does not work as expected - Shows signature but also shows the query if no signature is there.
-        case 'page_last_rev_sig':
-            #select * from page_verification where page_title='Main Page' AND signature <> '' ORDER BY rev_id DESC;
-            $page_title = $var1;
-            /** Database Query */
-            $lb = MediaWikiServices::getInstance()->getDBLoadBalancer();
-            $dbr = $lb->getConnectionRef( DB_REPLICA );
-            $res = $dbr->select(
-                'page_verification',
-                [ 'rev_id','page_title','signature' ],
-                'page_title=\''.$page_title.'\'',
-                __METHOD__,
-                [ 'ORDER BY' => 'signature' ] 
-            );
-
-            $output = '';
-            foreach( $res as $row ) {
-                $output = 'Page Title: ' . $row->page_title .' Signature: ' . $row->signature . ' Revision_ID: ' . $row->rev_id;  
-            }                                 
-            return [$output];
-
             #Expects Page Title and returns ALL verified revisions
             #NOT IMPLEMENTED
         case 'page_all_rev':
             $page_title = $var1;
             return get_page_all_rev($page_title);
-
-            #Expects Page Title and returns ALL verified revisions which have been signed
-        case 'get_page_all_rev_sig':
-            return ['NOT IMPLEMENTED'];
 
             #request_merkle_proof:expects witness_id and page_verification hash and returns left_leaf,righ_leaf and successor hash to verify the merkle proof node by node, data is retrieved from the witness_merkle_tree db. Note: in some cases there will be multiple replays to this query. In this case it is required to use the depth as a selector to go through the different layers. Depth can be specified via the $depth parameter; 
         case 'request_merkle_proof':
@@ -240,10 +205,6 @@ class StandardRestApi extends SimpleHandler {
                 "rev_id =$rev_id");
 
             return ( "Successfully stored Data for Revision[{$var1}] in Database! Data: Signature[{$var2}], Public_Key[{$var3}] and Wallet_Address[{$var4}] "  );
-            $signature=[];
-            $public_key=[];
-            $wallet_address=[];
-            $rev_id=[];
 
             #Expects all required input for the page_witness database: Transaction Hash, Sender Address, List of Pages with witnessed revision
         case 'store_witness_tx':
