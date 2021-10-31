7a9cf049557a16bafc13e53daffb6fd6e7e677a1
rht <rhtbot@protonmail.com>
2021-10-31 05:16:13 -0400

    Add GH Actions for API testing

---
+++ .github/workflows/installWiki.sh
@@ -0,0 +1,49 @@
+#! /bin/bash
+
+set -e
+
+# Taken from https://github.com/ProfessionalWiki/Maps/blob/master/.github/workflows/installMediaWiki.sh
+# Modifications:
+# 1. Set wgScriptPath to ""
+
+MW_BRANCH=$1
+EXTENSION_NAME=$2
+
+wget "https://github.com/wikimedia/mediawiki/archive/$MW_BRANCH.tar.gz" -nv
+
+tar -zxf $MW_BRANCH.tar.gz
+mv mediawiki-$MW_BRANCH mediawiki
+
+cd mediawiki
+
+composer install
+php maintenance/install.php \
+	--dbtype sqlite \
+	--dbuser root \
+	--dbname mw \
+	--dbpath "$(pwd)" \
+	--scriptpath="" \
+	--pass AdminPassword WikiName AdminUser
+
+echo 'error_reporting(E_ALL| E_STRICT);' >> LocalSettings.php
+echo 'ini_set("display_errors", 1);' >> LocalSettings.php
+echo '$wgShowExceptionDetails = true;' >> LocalSettings.php
+echo '$wgShowDBErrorBacktrace = true;' >> LocalSettings.php
+echo '$wgDevelopmentWarnings = true;' >> LocalSettings.php
+
+echo 'wfLoadExtension( "'$EXTENSION_NAME'" );' >> LocalSettings.php
+
+cat <<EOT >> composer.local.json
+{
+  "require": {
+  },
+	"extra": {
+		"merge-plugin": {
+			"merge-dev": true,
+			"include": [
+				"extensions/$EXTENSION_NAME/composer.json"
+			]
+		}
+	}
+}
+EOT
