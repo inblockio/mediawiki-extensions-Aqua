51211b3dfada481fb51d6d8b11d39ffe508b686f
Jeroen De Dauw <jeroendedauw@gmail.com>
2021-11-10 14:37:08 +0100

    Switch from spaces to tabs

--- tests/phpunit/integration/APIRESTRead.php
+++ tests/phpunit/integration/APIRESTRead.php
@@ -10,29 +10,29 @@ use MediaWiki\Tests\Rest\Handler\HandlerTestTrait;
 use MediaWikiIntegrationTestCase;
 
 class DAApiTest extends MediaWikiIntegrationTestCase {
-    use HandlerTestTrait;
+	use HandlerTestTrait;
 
-    public function testVerifyPage(): void {
-        $response = $this->executeHandler(
-            new VerifyPageHandler(),
-            new RequestData( [ 'pathParams' => [ 'rev_id' => '2' ] ] )
-        );
+	public function testVerifyPage(): void {
+		$response = $this->executeHandler(
+			new VerifyPageHandler(),
+			new RequestData( [ 'pathParams' => [ 'rev_id' => '2' ] ] )
+		);
 
-        $this->assertSame( 'application/json', $response->getHeaderLine( 'Content-Type' ) );
-        $data = json_decode( $response->getBody()->getContents(), true );
-        $this->assertIsArray( $data, 'Body must be a JSON array' );
-        $keys = [
-            "rev_id",
-            "domain_id",
-            "verification_hash",
-            "time_stamp",
-            "signature",
-            "public_key",
-            "wallet_address",
-            "witness_event_id",
-        ];
-        foreach ($keys as $key) {
-            $this->assertArrayHasKey( $key, $data );
-        }
-    }
+		$this->assertSame( 'application/json', $response->getHeaderLine( 'Content-Type' ) );
+		$data = json_decode( $response->getBody()->getContents(), true );
+		$this->assertIsArray( $data, 'Body must be a JSON array' );
+		$keys = [
+			"rev_id",
+			"domain_id",
+			"verification_hash",
+			"time_stamp",
+			"signature",
+			"public_key",
+			"wallet_address",
+			"witness_event_id",
+		];
+		foreach ($keys as $key) {
+			$this->assertArrayHasKey( $key, $data );
+		}
+	}
 }
