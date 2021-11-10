9469b4e8f8044ccf087064b212e8124b789c6334
rht <rhtbot@protonmail.com>
2021-11-09 21:51:44 -0500

    CI: Switch from api-testing to MediaWiki php test

---
+++ tests/phpunit/integration/APIRESTRead.php
@@ -0,0 +1,34 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace DataAccounting\Tests;
+
+use MediaWiki\Rest\RequestData;
+use DataAccounting\API\VerifyPageHandler;
+
+class DAApiTest extends MediaWikiIntegrationTestCase {
+    public function testVerifyPage(): void {
+        $response = $this->executeHandler(
+            new VerifyPageHandler(),
+            new RequestData( [ 'pathParams' => [ 'rev_id' => '2' ] ] )
+        );
+
+        $this->assertSame( 'application/json', $response->getHeaderLine( 'Content-Type' ) );
+        $data = json_decode( $response->getBody()->getContents(), true );
+        $this->assertIsArray( $data, 'Body must be a JSON array' );
+        $keys = [
+            "rev_id",
+            "domain_id",
+            "verification_hash",
+            "time_stamp",
+            "signature",
+            "public_key",
+            "wallet_address",
+            "witness_event_id",
+        ];
+        foreach ($keys as $key) {
+            $this->assertArrayHasKey( $key, $data );
+        }
+    }
+}
