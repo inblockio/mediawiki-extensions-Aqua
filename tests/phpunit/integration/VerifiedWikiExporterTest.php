2dd80d51fa25f1a71258a133c349c92899e76b02
Jeroen De Dauw <jeroendedauw@gmail.com>
2021-11-14 18:01:03 +0100

    Add VerifiedWikiExporterTest

---
+++ tests/phpunit/integration/VerifiedWikiExporterTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace DataAccounting\Tests;
+
+use DataAccounting\VerifiedWikiExporter;
+use MediaWikiIntegrationTestCase;
+
+/**
+ * @group Database
+ * @covers \DataAccounting\VerifiedWikiExporter
+ */
+class VerifiedWikiExporterTest extends MediaWikiIntegrationTestCase {
+
+	public function testVerificationDataIsPresentInOutput(): void {
+		$exporter = new VerifiedWikiExporter(
+			$this->db
+		);
+
+		$output = new \DumpStringOutput();
+		$exporter->sink = $output;
+
+		$title = $this->insertPage( 'ExportTestPage' )['title'];
+
+		$exporter->pageByTitle( $title );
+
+		$this->assertStringContainsString(
+			'<data_accounting_chain_height>1</data_accounting_chain_height>',
+			$output->__toString()
+		);
+
+		$this->assertStringContainsString(
+			'<verification>',
+			$output->__toString()
+		);
+
+		$this->assertStringContainsString(
+			'<verification_hash>',
+			$output->__toString()
+		);
+	}
+
+}
