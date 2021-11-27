a247f1b02240f6c5b3306d41a4b95f885a303e23
rht <rhtbot@protonmail.com>
2021-11-27 02:32:58 -0500

    test: 💍 Add get_page_last_rev test; improve get_page_by_rev_id

--- tests/phpunit/integration/DAApiTest.php
+++ tests/phpunit/integration/DAApiTest.php
@@ -12,6 +12,7 @@ use MediaWiki\Rest\HttpException;
 use DataAccounting\API\VerifyPageHandler;
 use DataAccounting\API\GetPageAllRevsHandler;
 use DataAccounting\API\GetPageByRevIdHandler;
+use DataAccounting\API\GetPageLastRevHandler;
 
 /**
  * @group Database
@@ -86,17 +87,17 @@ class DAApiTest extends MediaWikiIntegrationTestCase {
 		);
 		$this->assertJsonContentType( $response );
 		$data = $this->getJsonBody( $response );
-		$this->assertSame( $data, [ [
+		$this->assertSame( [ [
 			'page_title' => 'UTPage',
 			'page_id' => '1',
 			'rev_id' => '1'
-		] ] );
+		] ], $data );
 	}
 
 	/**
-	 * @covers \DataAccounting\API\GetPageAllRevsHandler
+	 * @covers \DataAccounting\API\GetPageByRevIdHandler
 	 */
-	public function testGetPageByRevIdHandler(): void {
+	public function testGetPageByRevId(): void {
 		// Testing the case when the rev_id is not found.
 		try {
 			$response = $this->executeHandler(
@@ -106,5 +107,64 @@ class DAApiTest extends MediaWikiIntegrationTestCase {
 		} catch ( HttpException $ex ) {
 			$this->assertSame( 'rev_id not found in the database', $ex->getMessage() );
 		}
+
+		// Testing the case when the rev_id is found.
+		$response = $this->executeHandler(
+			new VerifyPageHandler(),
+			new RequestData( [ 'pathParams' => [ 'rev_id' => '1' ] ] )
+		);
+		$this->assertJsonContentType( $response );
+		$data = $this->getJsonBody( $response );
+		$this->assertIsArray( $data, 'Body must be a JSON array' );
+		$keys = [
+			'rev_id',
+			'domain_id',
+			'verification_hash',
+			'time_stamp',
+			'signature',
+			'public_key',
+			'wallet_address',
+			'witness_event_id',
+		];
+		foreach ( $keys as $key ) {
+			$this->assertArrayHasKey( $key, $data );
+		}
+		$this->assertSame( 1, $data['rev_id'] );
+	}
+
+	/**
+	 * @covers \DataAccounting\API\GetPageLastRevHandler
+	 */
+	public function testGetPageLastRev(): void {
+		// Testing the case when the rev_id is not found.
+		try {
+			$response = $this->executeHandler(
+				new GetPageLastRevHandler(),
+				new RequestData( [ 'pathParams' => [ 'page_title' => 'IDONTEXIST IDONTEXIST' ] ] )
+			);
+		} catch ( HttpException $ex ) {
+			$this->assertSame( 'page_title not found in the database', $ex->getMessage() );
+		}
+
+		// Testing the case when the rev_id not found.
+		$response = $this->executeHandler(
+			new GetPageLastRevHandler(),
+			new RequestData( [ 'pathParams' => [ 'page_title' => 'UTPage' ] ] )
+		);
+		$this->assertJsonContentType( $response );
+		$data = $this->getJsonBody( $response );
+		$this->assertIsArray( $data, 'Body must be a JSON array' );
+		$keys = [
+			'page_title',
+			'page_id',
+			'rev_id',
+			'verification_hash',
+		];
+		foreach ( $keys as $key ) {
+			$this->assertArrayHasKey( $key, $data );
+		}
+		// TODO the rev_id shouldn't be a string.
+		$this->assertSame( '1', $data['rev_id'] );
+		$this->assertSame( 'UTPage', $data['page_title'] );
 	}
 }
