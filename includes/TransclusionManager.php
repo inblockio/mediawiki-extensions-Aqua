b603075ab34ac82ae098e9a593dba38678e18900
d.savuljesku <d.savuljesku@gmail.com>
2021-12-14 10:35:36 +0100

    feat: Hash files and include it in content hash

--- includes/TransclusionManager.php
+++ includes/TransclusionManager.php
@@ -14,6 +14,7 @@ use MediaWiki\Storage\RevisionStore;
 use MediaWiki\User\UserIdentity;
 use Title;
 use TitleFactory;
+use const http\Client\Curl\VERSIONS;
 
 class TransclusionManager {
 	public const STATE_NEW_VERSION = 'new-version';
