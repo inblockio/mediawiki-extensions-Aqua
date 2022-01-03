a8853d3f66359a1f2ae222d7b4342bd76d54007a
pwirth <wirth@hallowelt.biz>
2022-01-03 13:50:44 +0100

    Add GetHashChainInfoHandler rest api handler integration test

--- tests/phpunit/integration/DAApiTest.php
+++ tests/phpunit/integration/DAApiTest.php
@@ -16,8 +16,10 @@ use DataAccounting\API\GetPageLastRevHandler;
 use DataAccounting\API\RequestHashHandler;
 use DataAccounting\API\VerifyPageHandler;
 use DataAccounting\API\WriteStoreSignedTxHandler;
+use DataAccounting\API\GetHashChainInfoHandler;
 
 use DataAccounting\ServerInfo;
+use DataAccounting\Verification\Entity\VerificationEntity;
 use DataAccounting\API\GetServerInfoHandler;
 
 /**
@@ -48,6 +50,125 @@ class DAApiTest extends MediaWikiIntegrationTestCase {
 		$this->assertIsArray( $data, 'Body must be a JSON array' );
 		$this->assertArrayHasKey( 'api_version', $data );
 		$this->assertSame( ServerInfo::DA_API_VERSION, $data['api_version'] );
+	}
+
+	/**
+	 * @covers \DataAccounting\API\GetHashChainInfoHandler
+	 */
+	public function testHashChainInfoHandler(): void {
+		// Testing the case when the rev_id is found.
+		$services = [
+			$this->getServiceContainer()->getPermissionManager(),
+			$this->getServiceContainer()->getService( 'DataAccountingVerificationEngine' ),
+			$this->getServiceContainer()->getContentLanguage(),
+			$this->getServiceContainer()->getNamespaceInfo(),
+		];
+		$requestData = new RequestData( [ 'pathParams' => [ 'id_type' => 'title', 'id' => 'UTPage' ] ] );
+		// Should be denied permission unless the user is authorized.
+		$this->expectContextPermissionDenied(
+			new GetHashChainInfoHandler( ...$services ),
+			$requestData
+		);
+		// Let's authorize the user. Because this API endpoint requires 'read'
+		// permission for the title that is related to given rev_id.
+		$services[0] = $this->createMock( PermissionManager::class );
+		$services[0]->method( 'userCan' )->willReturn( true );
+		$response = $this->executeHandler(
+			new GetHashChainInfoHandler( ...$services ),
+			$requestData
+		);
+
+		$this->assertJsonContentType( $response );
+		$data = $this->getJsonBody( $response );
+		$this->assertIsArray( $data, 'Body must be a JSON array' );
+		$keys = [
+			VerificationEntity::GENESIS_HASH,
+			VerificationEntity::DOMAIN_ID,
+			'latest_verification_hash',
+			'site_info',
+			'title',
+			'namespace',
+			'chain_height',
+		];
+		foreach ( $keys as $key ) {
+			$this->assertArrayHasKey( $key, $data );
+		}
+
+		// Testing the case when the rev_id is not found.
+		$this->expectExceptionObject(
+			new HttpException( "Not found", 404 )
+		);
+
+		$response = $this->executeHandler(
+			new GetHashChainInfoHandler( ...$services ),
+			new RequestData( [ 'pathParams' => [ 'id_type' => 'title', 'id' => 'Test' ] ] )
+		);
+
+		$response = $this->executeHandler(
+			new GetHashChainInfoHandler( ...$services ),
+			$requestData
+		);
+
+		$this->assertJsonContentType( $response );
+		$data = $this->getJsonBody( $response );
+		$this->assertIsArray( $data, 'Body must be a JSON array' );
+		$keys = [
+			VerificationEntity::GENESIS_HASH,
+			VerificationEntity::DOMAIN_ID,
+			'latest_verification_hash',
+			'site_info',
+			'title',
+			'namespace',
+			'chain_height',
+		];
+		foreach ( $keys as $key ) {
+			$this->assertArrayHasKey( $key, $data );
+		}
+
+		// Testing the case when the title is not found.
+		$this->expectExceptionObject(
+			new HttpException( "Not found", 404 )
+		);
+
+		$response = $this->executeHandler(
+			new GetHashChainInfoHandler( ...$services ),
+			new RequestData( [ 'pathParams' => [ 'id_type' => 'title', 'id' => 'Test' ] ] )
+		);
+		
+		// --------------------
+		$requestData = new RequestData( [ 'pathParams' => [
+			'id_type' => 'genisis_hash',
+			'id' => '7ed23e2cfaf58e2e9c23a01378f377a09366d52fa262776551ce8221962cd49efbfce462473072c8592e21b3e2d07118e8fd79a37131696d719fe37404cade1b'
+		] ] );
+		$response = $this->executeHandler(
+			new GetHashChainInfoHandler( ...$services ),
+			$requestData
+		);
+
+		$this->assertJsonContentType( $response );
+		$data = $this->getJsonBody( $response );
+		$this->assertIsArray( $data, 'Body must be a JSON array' );
+		$keys = [
+			VerificationEntity::GENESIS_HASH,
+			VerificationEntity::DOMAIN_ID,
+			'latest_verification_hash',
+			'site_info',
+			'title',
+			'namespace',
+			'chain_height',
+		];
+		foreach ( $keys as $key ) {
+			$this->assertArrayHasKey( $key, $data );
+		}
+		// Testing the case when the genisis_hash is not found.
+		$this->expectExceptionObject(
+			new HttpException( "Not found", 404 )
+		);
+
+		$response = $this->executeHandler(
+			new GetHashChainInfoHandler( ...$services ),
+			new RequestData( [ 'pathParams' => [ 'id_type' => 'genisis_hash', 'id' => '123' ] ] )
+		);
 	}
 
 	/**
