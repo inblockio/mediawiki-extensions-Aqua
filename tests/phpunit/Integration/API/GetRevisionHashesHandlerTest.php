79878b4abf997ebedc461418a5dd4427a1aef2c5
pwirth <wirth@hallowelt.biz>
2022-01-21 12:19:20 +0100

    Tests: Fix broken api tests

--- tests/phpunit/Integration/API/GetRevisionHashesHandlerTest.php
+++ tests/phpunit/Integration/API/GetRevisionHashesHandlerTest.php
@@ -6,10 +6,12 @@ namespace DataAccounting\Tests\Integration\API;
 
 use DataAccounting\API\GetRevisionHashesHandler;
 use DataAccounting\Tests\Integration\API;
+use DataAccounting\Verification\Entity\VerificationEntity;
 use MediaWiki\Rest\RequestData;
 use MediaWiki\Tests\Rest\Handler\HandlerTestTrait;
 use MediaWiki\Rest\HttpException;
 use MediaWiki\Permissions\PermissionManager;
+use Title;
 
 /**
  * @group Database
@@ -21,13 +23,17 @@ class GetRevisionHashesHandlerTest extends API {
 	 * @covers \DataAccounting\API\GetRevisionHashesHandler
 	 */
 	public function testGetRevisionHashesHandler(): void {
-		// Testing the case when the page is found.
+		$vEngine = $this->getServiceContainer()->getService( 'DataAccountingVerificationEngine' );
+		$title = Title::newFromText( 'UTPage' );
+		$entity = $vEngine->getLookup()->verificationEntityFromTitle( $title );
 		$services = [
 			$this->getServiceContainer()->getPermissionManager(),
 			$this->getServiceContainer()->getService( 'DataAccountingVerificationEngine' ),
 		];
 		$requestData = new RequestData( [ 'pathParams' => [
-			'verification_hash' => '6dc8489cf860aab2bf34a24c366a3c3767e47943bfc2bca6661bb39d2546d2f5a246e5518b7161b61fa5639c767bc551b3845781f1c01ec4988192959a700b94'
+			VerificationEntity::VERIFICATION_HASH => $entity->getHash(
+				VerificationEntity::VERIFICATION_HASH
+			),
 		] ] );
 		$this->expectContextPermissionDenied(
 			new GetRevisionHashesHandler( ...$services ),
@@ -41,22 +47,15 @@ class GetRevisionHashesHandlerTest extends API {
 			new GetRevisionHashesHandler( ...$services ),
 			$requestData
 		);
+
 		$this->assertJsonContentType( $response );
 		$data = $this->getJsonBody( $response );
 		$this->assertIsArray( $data, 'Body must be a JSON array' );
-		$keys = [
-			'verification_context',
-			'content',
-			'metadata',
-			'signature',
-			'witness',
-		];
-		foreach ( $keys as $key ) {
-			$this->assertArrayHasKey( $key, $data );
-		}
+		$this->assertArrayHasKey( 0, $data );
 		// TODO the rev_id shouldn't be a string.
-		#$this->assertSame( 1, $data['rev_id'] );
-		#$this->assertSame( 'UTPage', $data['page_title'] );
+		$this->assertSame( $data[0], $entity->getHash(
+			VerificationEntity::VERIFICATION_HASH
+		) );
 
 		// Testing the case when the page is not found.
 		$this->expectExceptionObject(
