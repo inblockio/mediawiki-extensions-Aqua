698d8af69dafcc56cf1dd70f05ba2372d0e44700
d.savuljesku <d.savuljesku@gmail.com>
2023-08-03 10:25:10 +0200

    Fix Content deprecations

--- includes/Content/SignatureContent.php
+++ includes/Content/SignatureContent.php
@@ -2,78 +2,19 @@
 
 namespace DataAccounting\Content;
 
-use Html;
 use JsonContent;
-use MediaWiki\MediaWikiServices;
 use Message;
-use ParserOptions;
-use ParserOutput;
-use Title;
 
 class SignatureContent extends JsonContent implements DataAccountingContent {
 	public const CONTENT_MODEL_SIGNATURE = 'signature';
 	public const SLOT_ROLE_SIGNATURE = 'signature-slot';
 
-	/** @var \MediaWiki\User\UserFactory  */
-	private $userFactory;
-
 	public function __construct( $text ) {
 		parent::__construct( $text, static::CONTENT_MODEL_SIGNATURE );
-		$this->userFactory = MediaWikiServices::getInstance()->getUserFactory();
-	}
-
-	protected function fillParserOutput(
-		Title $title, $revId, ParserOptions $options, $generateHtml, ParserOutput &$output
-	) {
-		if ( !$this->isValid() ) {
-			return '';
-		}
-		$data = json_decode( $this->getText(), 1 );
-
-		$output->setText( $this->getSignatureContent( $data ) );
 	}
 
 	public function isValid() {
 		return $this->getText() === '' || parent::isValid();
-	}
-
-	private function getSignatureContent( array $data ) {
-		$signatures = '';
-		foreach ( $data as $signature ) {
-			if ( !is_array( $signature ) ) {
-				continue;
-			}
-			$syntaxForPage = $this->getSignatureLine( $signature );
-			if ( !$syntaxForPage ) {
-				$signatures .= 'Invalid signature';
-			} else {
-				$signatures .= $syntaxForPage;
-			}
-
-			$signatures .= '<br>';
-		}
-		return Html::rawElement( 'div', [], $signatures );
-	}
-
-	private function getSignatureLine( array $signature ) {
-		if ( !isset( $signature['user'] ) || !isset( $signature['timestamp'] ) ) {
-			return null;
-		}
-		$user = $this->userFactory->newFromName( $signature['user'] );
-		if ( !$user ) {
-			return null;
-		}
-		$linker = MediaWikiServices::getInstance()->getLinkRenderer();
-		$line = $linker->makeLink( $user->getUserPage(), $user->getName() );
-
-		$language = \RequestContext::getMain()->getLanguage();
-		$viewingUser = \RequestContext::getMain()->getUser();
-		$line .= Html::rawElement( 'span', [
-			'class' => 'badge badge-light',
-			'style' => 'margin-left: 10px',
-		], $language->userTimeAndDate( $signature['timestamp'], $viewingUser ) );
-
-		return Html::rawElement( 'span', [ 'class' => 'da-signature-line' ], $line );
 	}
 
 	/**
--- includes/ContentHandler/SignatureHandler.php
+++ includes/ContentHandler/SignatureHandler.php
@@ -2,12 +2,25 @@
 
 namespace DataAccounting\ContentHandler;
 
+use Content;
 use DataAccounting\Content\SignatureContent;
+use Html;
 use JsonContentHandler;
+use MediaWiki\Content\Renderer\ContentParseParams;
+use MediaWiki\MediaWikiServices;
+use MediaWiki\User\UserFactory;
+use ParserOutput;
 
 class SignatureHandler extends JsonContentHandler {
+
+	/**
+	 * @var UserFactory
+	 */
+	private $userFactory;
+
 	public function __construct() {
 		parent::__construct( SignatureContent::CONTENT_MODEL_SIGNATURE );
+		$this->userFactory = MediaWikiServices::getInstance()->getUserFactory();
 	}
 
 	/**
@@ -50,5 +63,72 @@ class SignatureHandler extends JsonContentHandler {
 	 */
 	public function supportsDirectEditing() {
 		return false;
+	}
+
+	/**
+	 * @param SignatureContent $content
+	 * @param ContentParseParams $cpoParams
+	 * @param ParserOutput $parserOutput
+	 *
+	 * @return void
+	 */
+	protected function fillParserOutput(
+		Content $content, ContentParseParams $cpoParams, ParserOutput &$parserOutput
+	) {
+		if ( !$content->isValid() ) {
+			return;
+		}
+		$data = json_decode( $content->getText(), 1 );
+
+		$parserOutput->setText( $this->getSignatureContent( $data ) );
+	}
+
+	/**
+	 * @param array $data
+	 *
+	 * @return string
+	 */
+	private function getSignatureContent( array $data ) {
+		$signatures = '';
+		foreach ( $data as $signature ) {
+			if ( !is_array( $signature ) ) {
+				continue;
+			}
+			$syntaxForPage = $this->getSignatureLine( $signature );
+			if ( !$syntaxForPage ) {
+				$signatures .= 'Invalid signature';
+			} else {
+				$signatures .= $syntaxForPage;
+			}
+
+			$signatures .= '<br>';
+		}
+		return Html::rawElement( 'div', [], $signatures );
+	}
+
+	/**
+	 * @param array $signature
+	 *
+	 * @return string|null
+	 */
+	private function getSignatureLine( array $signature ) {
+		if ( !isset( $signature['user'] ) || !isset( $signature['timestamp'] ) ) {
+			return null;
+		}
+		$user = $this->userFactory->newFromName( $signature['user'] );
+		if ( !$user ) {
+			return null;
+		}
+		$linker = MediaWikiServices::getInstance()->getLinkRenderer();
+		$line = $linker->makeLink( $user->getUserPage(), $user->getName() );
+
+		$language = \RequestContext::getMain()->getLanguage();
+		$viewingUser = \RequestContext::getMain()->getUser();
+		$line .= Html::rawElement( 'span', [
+			'class' => 'badge badge-light',
+			'style' => 'margin-left: 10px',
+		], $language->userTimeAndDate( $signature['timestamp'], $viewingUser ) );
+
+		return Html::rawElement( 'span', [ 'class' => 'da-signature-line' ], $line );
 	}
 }
